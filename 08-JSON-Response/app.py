9dc99430f35513688729d4a211429e58a132bb2d
Vijayaprakash P <msgtovp@gmail.com>
2018-05-18 13:56:10 +0530

    JSON Response Added

---
+++ 08-JSON-Response/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("landing.html")
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return "This is contact page"
+
+@app.route("/success")
+def login_success():
+    return "Login Success"
+
+@app.route("/error")
+def login_error():
+    return "Login Error"
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    print(request.headers)
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        req = {'username': request.form["user"], 'password': request.form["pass"]}
+        data = {'username': 'steve', 'password': 'jobs'}
+        if data == req:
+            return redirect(url_for('login_success'))
+        else:
+            return redirect(url_for('login_error'))
+
+@app.route("/users/all")
+def get_users():
+    users = [
+        {'user':'guido', 'pass': 'rossum', 'fullname': 'Guido Van Rossum'},
+        {'user':'james', 'pass': 'gosling', 'fullname': 'James Gosling'},
+        {'user':'frank', 'pass': 'edust', 'fullname': 'EdFrank'},
+        {'user':'chrish', 'pass': 'worth', 'fullname': 'Chrish Worth'},
+        {'user':'patrick', 'pass': 'naughton', 'fullname': 'Patrick Naughton'}
+    ]
+    return jsonify(users)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
